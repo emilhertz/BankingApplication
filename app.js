25ea4e93fec23aa8a2333736b94001e01741f009
Emil Hertz <emil.hertz@gmail.com>
2020-11-25 13:33:32 +0100

    SSL implementation

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const app = express();
 const bodyParser = require("body-parser");
 const accountRoute = require("./routes/accountsRoutes");
 const clientRoute = require("./routes/clientRoutes");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+
+//ssl work
+const server = https
+  .createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, "SSL", "key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, "SSL", "cert.pem")),
+    },
+    app
+  )
+  .listen(3443, () => {
+    console.log("Server listening on port 3443");
+  });
 
 //Added Body-parser middleware
 app.use(bodyParser.json());
@@ -11,12 +27,6 @@ app.use(bodyParser.json());
 app.use("/accounts", accountRoute);
 app.use("/clients", clientRoute);
 
-//Replace with loadbalancer
-app.listen(8080, () => {
-  console.log("Server listening on 8080");
-});
-
-//needs SSL implementation
 //needs load-balancer implementation
 //needs one-click-run solution for easy excecution
 //needs to pass test
